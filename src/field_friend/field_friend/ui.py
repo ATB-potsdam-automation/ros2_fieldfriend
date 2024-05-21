4b5c84288d7714b83ff45733bd061705b116b8db
Tjark <tschuette@atb-potsdam.de>
2024-05-21 15:13:16 +0200

    adjusted twist message from ui

--- src/field_friend/field_friend/ui.py
+++ src/field_friend/field_friend/ui.py
@@ -9,7 +9,7 @@ import json
 class UINode(Node):
     def __init__(self):
         super().__init__('ui')
-        self.publish_twist = self.create_publisher(Twist, '/steer', 1)
+        self.publish_twist = self.create_publisher(Twist, '/cmd_vel', 1)
         self.publish_configure = self.create_publisher(Empty, '/configure', 1)
         self.subscription_status = self.create_subscription(String, '/status', self.handle_status, 1)
         self.subscription_odometry = self.create_subscription(Twist, '/odometry', self.handle_odometry, 1)
@@ -26,7 +26,7 @@ class UINode(Node):
                 ui.joystick(
                     color='blue',
                     size=50,
-                    on_move=lambda msg: self.send(msg.data.vector.x, msg.data.vector.y),
+                    on_move=lambda msg: self.send(msg.x, msg.y), #self.get_logger().info(msg),
                     on_end=lambda _: self.send(0, 0),
                 )
             with ui.card() as odometry:
@@ -41,9 +41,10 @@ class UINode(Node):
         ui.run(title='ROS Example', reload=False, show=False)
 
     def send(self, x, y):
-        linear = Vector3(y, 0, 0)
-        angular = Vector3(0, 0, -x)
-        self.publish_twist.publish(Twist(linear, angular))
+        cmd_message = Twist()
+        cmd_message.linear.x, cmd_message.linear.y, cmd_message.linear.z = float(y), 0.0, 0.0
+        cmd_message.angular.x, cmd_message.angular.y, cmd_message.angular.z = 0.0, 0.0, float(-x)
+        self.publish_twist.publish(cmd_message)
 
     def handle_status(self, data):
         msg = json.loads(data.data)
