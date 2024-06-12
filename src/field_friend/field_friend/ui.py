4d10a50d78493dfacf3cdfc4c7a460c3233adb19
Tjark <tschuette@atb-potsdam.de>
2024-06-12 17:39:40 +0200

    changed ui now displaying position based on global odometry (gps)

--- src/field_friend/field_friend/ui.py
+++ src/field_friend/field_friend/ui.py
@@ -1,66 +1,76 @@
-#!/usr/bin/env python3
+import math
+import threading
+from pathlib import Path
+
 import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Empty, String
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Pose, Twist
 from nav_msgs.msg import Odometry
-from nicegui import ui
-import json
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
 
-class UINode(Node):
-    def __init__(self):
-        super().__init__('ui')
-        self.publish_twist = self.create_publisher(Twist, '/cmd_vel', 1)
-        self.publish_configure = self.create_publisher(Empty, '/configure', 1)
-        self.subscription_status = self.create_subscription(String, '/status', self.handle_status, 1)
-        self.subscription_odometry = self.create_subscription(Odometry, '/odometry', self.handle_odometry, 1)
+from nicegui import Client, app, ui, ui_run
 
-        with ui.row().classes('items-stretch'):
-            with ui.card():
-                ui.markdown('### Setup')
-                ui.button('Configure', on_click=lambda: self.publish_configure.publish(Empty()))
-            with ui.card() as status:
-                ui.markdown('### Status')
-                self.time = ui.label()
-            with ui.card():
-                ui.markdown('### Control')
-                ui.joystick(
-                    color='blue',
-                    size=50,
-                    on_move=lambda msg: self.send(msg.x, msg.y), #self.get_logger().info(msg),
-                    on_end=lambda _: self.send(0, 0),
-                )
-            with ui.card() as odometry:
-                ui.markdown('### Odometry')
-                ui.label('linear')
-                self.linear = ui.slider(min=-1.0, max=1.0, step=0.01).props('label-always readonly')
-                ui.label('angular')
-                self.angular = ui.slider(min=-1.0, max=1.0, step=0.01).props('label-always readonly')
 
-            ui.timer(0.1, lambda: None)  # NOTE: update ui
+class NiceGuiNode(Node):
 
-        ui.run(title='ROS Example', reload=False, show=False)
+    def __init__(self) -> None:
+        super().__init__('nicegui')
+        self.cmd_vel_publisher = self.create_publisher(Twist, 'cmd_vel', 1)
+        self.subscription = self.create_subscription(Odometry, '/odometry/global', self.handle_pose, 1)
 
-    def send(self, x, y):
+        with Client.auto_index_client:
+            with ui.row().classes('items-stretch'):
+                with ui.card().classes('w-44 text-center items-center'):
+                    ui.label('Control').classes('text-2xl')
+                    ui.joystick(color='blue', size=50,
+                                on_move=lambda e: self.send_speed(float(e.x),float(e.y)),
+                                on_end=lambda _: self.send_speed(0.0, 0.0))
+                    ui.label('Publish steering commands by dragging your mouse around in the blue field').classes('mt-6')
+                with ui.card().classes('w-44 text-center items-center'):
+                    ui.label('Data').classes('text-2xl')
+                    ui.label('linear velocity').classes('text-xs mb-[-1.8em]')
+                    slider_props = 'readonly selection-color=transparent'
+                    self.linear = ui.slider(min=-1, max=1, step=0.05, value=0).props(slider_props)
+                    ui.label('angular velocity').classes('text-xs mb-[-1.8em]')
+                    self.angular = ui.slider(min=-1, max=1, step=0.05, value=0).props(slider_props)
+                    ui.label('position').classes('text-xs mb-[-1.4em]')
+                    self.position = ui.label('---')
+                with ui.card().classes('w-96 h-96 items-center'):
+                    ui.label('Visualization').classes('text-2xl')
+                    with ui.scene(350, 300) as scene:
+                        with scene.group() as self.robot_3d:
+                            prism = [[-0.5, -0.5], [0.5, -0.5], [0.75, 0], [0.5, 0.5], [-0.5, 0.5]]
+                            self.robot_object = scene.extrusion(prism, 0.4).material('#4488ff', 0.5)
+
+    def send_speed(self, y, x):
         cmd_message = Twist()
-        cmd_message.linear.x, cmd_message.linear.y, cmd_message.linear.z = 0.5 * float(y), 0.0, 0.0
-        cmd_message.angular.x, cmd_message.angular.y, cmd_message.angular.z = 0.0, 0.0, 0.5 * float(-x)
-        self.publish_twist.publish(cmd_message)
+        cmd_message.linear.x, cmd_message.linear.y, cmd_message.linear.z = 0.5 * float(x), 0.0, 0.0
+        cmd_message.angular.x, cmd_message.angular.y, cmd_message.angular.z = 0.0, 0.0, 0.5 * float(-y)
+        self.linear.value = x
+        self.angular.value = y
+        self.cmd_vel_publisher.publish(cmd_message)
 
-    def handle_status(self, data):
-        msg = json.loads(data.data)
-        self.get_logger().info("data.data")
-        self.time.text = f'Timestamp: {msg["time"]} ms'
 
-    def handle_odometry(self, data):
-        self.get_logger().info("received odometry")
-        self.linear.value = data.linear.x
-        self.angular.value = data.angular.z
+    def handle_pose(self, msg: Odometry) -> None:
+        self.position.text = f'x: {msg.pose.pose.position.x:.2f}, y: {msg.pose.pose.position.y:.2f}'
+        self.robot_3d.move(msg.pose.pose.position.x, msg.pose.pose.position.y)
+        self.robot_3d.rotate(0, 0, 2 * math.atan2(msg.pose.pose.orientation.z, msg.pose.pose.orientation.w))
 
-def main(args=None):
-    rclpy.init(args=args)
-    node = UINode()
-    rclpy.spin(node)
 
-if __name__ == '__main__':
-    main()
+def main() -> None:
+    # NOTE: This function is defined as the ROS entry point in setup.py, but it's empty to enable NiceGUI auto-reloading
+    pass
+
+
+def ros_main() -> None:
+    rclpy.init()
+    node = NiceGuiNode()
+    try:
+        rclpy.spin(node)
+    except ExternalShutdownException:
+        pass
+
+
+app.on_startup(lambda: threading.Thread(target=ros_main).start())
+ui_run.APP_IMPORT_STRING = f'{__name__}:app'  # ROS2 uses a non-standard module name, so we need to specify it here
+ui.run(uvicorn_reload_dirs=str(Path(__file__).parent.resolve()), favicon='🤖')
